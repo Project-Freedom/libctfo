147b16e85fb44e91e4b204f8d63e6f99447363ae
HowardPlayzOA <66976582+HPOA909@users.noreply.github.com>
2024-04-18 19:55:41 -0700

    bruhh

--- PyGame-control/__init__.py
+++ PyGame-control/__init__.py
@@ -1,5 +1,14 @@
 import pygame
+import sys, platform, tox
 
 class Control:
     def __init__(self):
-        self.forward=pygame
+        self.platformCheck=platform.python_version()
+        self.pygame=pygame
+        self.sys=sys
+    
+    def checker(self):
+        if (self.platformCheck >= 3.11.5):
+            print("Your Python version is fully compatible!")
+        else:
+            print("Your Python version must be 3.11.5 or above!")
